d810efbe4aa60370d5686edd10d8820e7335d90e
ElielCatauro <ecatauronardella@alumnos.exa.unicen.edu.ar>
2020-12-06 04:00:35 -0300

    filtros beta v2

--- assets/js/table_hard_main.js
+++ assets/js/table_hard_main.js
@@ -88,7 +88,7 @@ let modifiRow = (elemento, i) => {
 
 
 let createInputs = (claves) => {
-  inputsEl.innerHTML="";
+  inputsEl.innerHTML = "";
   for (let i = 0; i < claves.length; i++) {
     let divi = document.createElement('div');
     divi.setAttribute('class', `form-group row justify-content-between`);
@@ -169,7 +169,7 @@ let createBody = (elementos) => {
 
 
 let createTable = (claves, arregloObjet) => {
-  tableHardEl.innerHTML="";
+  tableHardEl.innerHTML = "";
   createHeader(claves);
   createBody(arregloObjet);
   createInputs(claves);
@@ -215,17 +215,134 @@ let loadGPUStable = () => {
 
 */
 
-let filtrosGPU = (incluyen1 ="",incluyen2 ="",min1 = 200,max1 = 30000,min2 = 0,max2 = 2000 ) => {
+let filtContEl = document.getElementById('filtroscont');
+
+let createFilInput = () => {
+  let theadEl = document.createElement("thead");
+  let trEl = document.createElement("tr");
+  let thEl1 = document.createElement("th");
+  let thEl2 = document.createElement("th");
+  let thEl3 = document.createElement("th");
+  let thEl4 = document.createElement("th");
+  let thEl5 = document.createElement("th");
+  let btnFil = document.createElement('button');
+  btnFil.setAttribute('class', `btn btn-secondary`);
+  btnFil.setAttribute("id", "filBTN");
+  btnFil.innerText = 'Filtrar';
+  let btnLimpia = document.createElement('button');
+  btnLimpia.setAttribute('class', `btn btn-secondary`);
+  btnLimpia.setAttribute("id", "btnLimpia");
+  btnLimpia.innerText = 'Limpiar';
+  thEl5.setAttribute('class', 'btn-group')
+  thEl5.appendChild(btnFil);
+  thEl5.appendChild(btnLimpia);
+
+  let filGPU_Name = document.createElement('input');
+  filGPU_Name.setAttribute('id', 'filGPU_Name');
+
+  let filTEST_Date = document.createElement('input');
+  filTEST_Date.setAttribute('id', 'filTEST_Date');
+
+  let filG3D_MarkMin = document.createElement('input');
+  filG3D_MarkMin.setAttribute('id', 'filG3D_MarkMin');
+  let filG3D_MarkMax = document.createElement('input');
+  filG3D_MarkMax.setAttribute('id', 'filG3D_MarkMax');
+
+  let filG2D_MarkMin = document.createElement('input');
+  filG2D_MarkMin.setAttribute('id', 'filG2D_MarkMin');
+  let filG2D_MarkMax = document.createElement('input');
+  filG2D_MarkMax.setAttribute('id', 'filG2D_MarkMax');
+
+  thEl1.innerHTML = 'contiene';
+  thEl1.appendChild(filGPU_Name);
+
+  thEl2.innerHTML = 'contiene';
+  thEl2.appendChild(filTEST_Date);
+
+  thEl3.innerHTML = 'min';
+  thEl3.appendChild(filG3D_MarkMin);
+  thEl3.innerHTML += 'max';
+  thEl3.appendChild(filG3D_MarkMax);
+
+  thEl4.innerHTML = 'min';
+  thEl4.appendChild(filG2D_MarkMin);
+  thEl4.innerHTML += 'max';
+  thEl4.appendChild(filG2D_MarkMax);
+
+  trEl.appendChild(thEl1);
+  trEl.appendChild(thEl2);
+  trEl.appendChild(thEl3);
+  trEl.appendChild(thEl4);
+  trEl.appendChild(thEl5);
+  filtContEl.appendChild(trEl);
+}
+createFilInput();
+
+let btnFiltraEL = document.getElementById('filBTN');
+
+let btnLimpiaTablaEL = document.getElementById('btnLimpia');
+
+let filGPU_NameEL = document.getElementById('filGPU_Name');
+
+let filTEST_DateEL = document.getElementById('filTEST_Date');
+
+let filG3D_MarkMinEL = document.getElementById('filG3D_MarkMin');
+let filG3D_MarkMaxEL = document.getElementById('filG3D_MarkMax');
+
+let filG2D_MarkMinEL = document.getElementById('filG2D_MarkMin');
+let filG2D_MarkMaxEL = document.getElementById('filG2D_MarkMax');
+
+
+
+
+let filtrosGPU = (incluyen1 = "", incluyen2 = "", min1 = 200, max1 = 30000, min2 = 0, max2 = 2000) => {
+  console.log('en el llamado ');
+  console.log(incluyen1);
+ console.log(incluyen2);
+ console.log( min1, max1);
+ console.log( min2, max2);
 
   let claves = Object.keys(dataParseada[0]);
-  let fil1=dataParseada.filter(e => e.GPU_Name.indexOf(`${incluyen1}`) !== -1 );
+  let fil1 = dataParseada.filter(e => e.GPU_Name.indexOf(`${incluyen1}`) !== -1);
+  let fil2 = fil1.filter(e => e.TEST_Date.indexOf(`${incluyen2}`) !== -1);
+  let fil3 = fil2.filter(e => (e.G3D_Mark >= min1 && e.G3D_Mark <= max1));
+  let fil4 = fil3.filter(e => (e.G2D_Mark >= min2 && e.G2D_Mark <= max2));  
   console.log(fil1);
-  let fil2=fil1.filter(e => e.TEST_Date.indexOf(`${incluyen2}`) !== -1 );
   console.log(fil2);
-  let fil3=fil2.filter(e => (e.G3D_Mark>= min1 && e.G3D_Mark<= max1 ));
-  console.log(fil3);
-  let fil4=fil3.filter(e => (e.G2D_Mark>=min2 && e.G2D_Mark<=max2 ));
-  console.log(fil4);
+  console.log( fil3);
+  console.log( fil4);
 
-  createTable(claves,fil4);
+  createTable(claves, fil4);
 }
+
+
+
+btnFiltraEL.addEventListener('click', () => {
+  
+  let incluyen1 = `${filGPU_NameEL.value}`;
+  let incluyen2 = `${filTEST_DateEL.value}`;
+  let min1 = parseInt(filG3D_MarkMinEL.value);
+  let max1 = parseInt(filG3D_MarkMaxEL.value);
+  let min2 = parseInt(filG2D_MarkMinEL.value);
+  let max2 = parseInt(filG2D_MarkMaxEL.value);
+
+  console.log(incluyen1);
+  console.log( incluyen2);
+  console.log( min1, max1);
+  console.log( min2, max2);
+  console.log('fuera del llamado ');
+
+  filtrosGPU(incluyen1, incluyen2, min1, max1, min2, max2);
+});
+
+
+
+btnLimpiaTablaEL.addEventListener('click', () => {
+  filtrosGPU();
+  filGPU_NameEL.value = "";
+  filTEST_DateEL.value = "";
+  filG3D_MarkMinEL.value = "";
+  filG3D_MarkMaxEL.value = "";
+  filG2D_MarkMinEL.value = "";
+  filG2D_MarkMaxEL.value = "";
+});
